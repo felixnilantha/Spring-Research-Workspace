98b17f823d237851cefb28c29de9da04cbe713a8
Nilantha Silva <felixnilantha@gmail.com>
2019-05-03 11:24:57 +1000

    init commit for spring rest crm crud example

--- 34.spring-rest-basics/src/main/java/com/spring/basics/rest/controller/StudentController.java
+++ 34.spring-rest-basics/src/main/java/com/spring/basics/rest/controller/StudentController.java
@@ -1,22 +1,17 @@
 package com.spring.basics.rest.controller;
 
-import java.rmi.StubNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.basics.rest.entity.Student;
-import com.spring.basics.rest.error.StudentErrorResponse;
 import com.spring.basics.rest.error.StudentNotFoundException;
 
 @RestController
