dbf85a1b9b49290cbb125992b681a3f1193d450f
Nilantha Silva <felixnilantha@gmail.com>
2019-05-09 14:04:18 +1000

    spring boot properties files overview

--- 38.spring-boot-application-dot-properties/src/main/java/com/springboot/basics/rest/BasicRestController.java
+++ 38.spring-boot-application-dot-properties/src/main/java/com/springboot/basics/rest/BasicRestController.java
@@ -1,25 +1,28 @@
 package com.springboot.basics.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicRestController {
+	
+	@Value("${student.name}")
+	private String studentName;
+	@Value("${student.subject}")
+	private String studentSubject;
 
-	
-	
-	
 	@GetMapping("/")
 	public String basic() {
-	
 		return "Spring Boot Basics  Start up page ...";
 	}
 	@GetMapping("/reload")
 	public String reload() {
 		return "Spring Boot Basics  Reloading the page ...";
 	}
-	
-	
 
-
+	@GetMapping("/team/info")
+	public String teamInfor() {
+		return "Student " + studentName + " subject is " + studentSubject;
+	}	
 }
